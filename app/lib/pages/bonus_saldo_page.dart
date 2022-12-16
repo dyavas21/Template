7f99cf0880b62280d29b2f2248dad21f0c9ab0c5
Gede Dyava S <dyavas21@gmail.com>
2022-12-16 19:36:28 +0700

    template lengkap

---
+++ app/lib/pages/bonus_saldo_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BonusSaldoPage extends StatelessWidget {
+  const BonusSaldoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
